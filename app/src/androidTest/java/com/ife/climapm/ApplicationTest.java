54a34005e106f715e463686dfd54771b4c2b6935
ifetilayo <ifetilayoade@gmail.com>
2019-08-24 11:11:38 -0500

    Changes

--- app/src/androidTest/java/com/ife/climapm/ApplicationTest.java
+++ app/src/androidTest/java/com/ife/climapm/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.climapm;
+package com.ife.climapm;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/ife/climapm/ExampleUnitTest.java
+++ app/src/test/java/com/ife/climapm/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.climapm;
+package com.ife.climapm;
 
 import org.junit.Test;
 
